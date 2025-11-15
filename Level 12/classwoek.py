898342b6cc80488a23eda78c5ed3a4ae9f56ed2f
lukazaqaidze <monkeypro933@gmail.com>
2025-11-15 19:14:04 +0400

    second commit

---
+++ Level 12/classwoek.py
@@ -0,0 +1,23 @@
+# 1
+# logical operators ეს არის ლოგიკური ოპერატორები რომლებსაც ლოგიკაზე დაყრდნობით გამოაქვთ დასკვნები და ესენია : 
+# და;ან კავშირები ანუ and;or  operators
+# ნებისმიერი შედარების ან ლოგიკური ოპერაცია პასუხად გვაძლევს boolean ტიპის მნიშვნელობას
+# # ლოგიკური ოპერატორები: and, or
+
+# and
+
+# print(True and True) # True
+# print(True and False) # False
+# print(False and True) # False
+# print(False and False) # False
+
+# print(True or True) # True
+# print(True or False) # True
+# print(False or True) # True
+# print(False or False) # False
+# 2
+# print(5 == 5 ) და ( 10 + 7 > 20) 
+
+# 3
+# (true and false or false or true)  #true
+# (false or true or false) true
---
+++ index.py/index.py
@@ -0,0 +1,18 @@
+# string = str (ამ სტრინგს სჭირდება ბრჭყალები ("")) "luka"
+# integer = int (ინტეჯერი არიის ყველა მთელი რიცხვი) 12
+# boolian = bool ( True  ან  False) 10>1 True 10< 1 False
+#float (ათწილადები ) 10.1  ან 10.0
+
+
+
+# print(2 * 8)
+# print(2 + 8)
+# print(2 - 8)
+# print(2 // 8)
+
+# my_age = 15
+# print(my_age) 
+
+
+
+
